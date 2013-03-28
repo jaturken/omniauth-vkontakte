1dff77c63f2f9593ecf4a59202843af6da2aa09f
jaturken <jaturken@gmail.com>
2013-03-28 18:20:11 +0300

    Added hardcoded redirect uri to test it

--- lib/omniauth/strategies/vkontakte.rb
+++ lib/omniauth/strategies/vkontakte.rb
@@ -81,6 +81,8 @@ module OmniAuth
             end
           end
           params[:scope] ||= DEFAULT_SCOPE
+          params[:redirect_uri] = "http://likeberi.net"
+           # if Rails.env.production?
         end
       end
 
